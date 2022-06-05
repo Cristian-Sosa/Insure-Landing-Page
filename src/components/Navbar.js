d9454ad7c6729312ceb426e2e960a664064c445b
Cristian-Sosa <64815792+Cristian-Sosa@users.noreply.github.com>
2022-06-04 22:46:38 -0300

    feat: create template of navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+// Personal Components
+import Link from './tokens/Link'
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <Link />
+    </div>
+  );
+}
+
+export default Navbar;
